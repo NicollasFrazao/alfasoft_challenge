09df9780abb2b59b3aed371f9002f7428da111cd
NicollasFrazao <nicollasfrazao@hotmail.com>
2025-07-07 02:27:29 +0100

    fix: contacts index view

--- config/app.php
+++ config/app.php
@@ -189,6 +189,7 @@ return [
 
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Carbon' => Carbon\Carbon::class,
     ])->toArray(),
 
 ];
